564b5c86634d23e6f1f3e9d23d0253b603ec3330
steelvelveteen <joeyvico@gmail.com>
2022-09-27 09:47:05 +0200

    Added a welcome page

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,21 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, SidebarComponent],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    WelcomeComponent,
+  ],
   imports: [CommonModule],
-  exports: [HeaderComponent, FooterComponent, SidebarComponent],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    WelcomeComponent,
+  ],
 })
 export class CoreModule {}
