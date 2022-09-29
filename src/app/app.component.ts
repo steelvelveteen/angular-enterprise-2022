3db77233da07d19405d88b0d111b40c72ee74ab9
steelvelveteen <joeyvico@gmail.com>
2022-09-29 14:55:40 +0200

    Getting started, again, with observables from scratch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
+// import { of } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-enterprise-2022';
+
+  constructor() {
+    const obs = new Observable();
+    console.log(obs);
+    const other = of(1, 2, 3);
+    other.subscribe((response) => console.log(response));
+  }
 }
