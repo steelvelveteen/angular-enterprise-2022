c8393d9c8ecd11494952e822e2ca35d8dd992f1e
joeyOnMac <joeyvico@gmail.com>
2022-10-01 21:28:49 +0200

    Amazing way to display the date time in real time with seconds moving

--- src/app/core/welcome/welcome.component.ts
+++ src/app/core/welcome/welcome.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss'],
 })
-export class WelcomeComponent {}
+export class WelcomeComponent {
+  time = new Observable<string>((observer: Observer<string>) => {
+    setInterval(() => observer.next(new Date().toString()), 1000);
+  });
+}
