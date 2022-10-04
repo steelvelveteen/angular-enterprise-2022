383f7aa34a703e098c5b6ed976e4df71e3d306b5
steelvelveteen <joeyvico@gmail.com>
2022-10-04 11:35:02 +0200

    Added barebone sidenav component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SampleComponent } from './components/sample/sample.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { ButtonComponent } from './components/ui/button/button.component';
 import { SampleUiComponent } from './components/ui/sample-ui/sample-ui.component';
 import { SampleDirectiveDirective } from './directives/sample-directive.directive';
@@ -16,6 +17,7 @@ import { LoadSpinnerComponent } from './ui/load-spinner/load-spinner.component';
     SampleDirectiveDirective,
     SamplePipePipe,
     LoadSpinnerComponent,
+    SidenavComponent,
   ],
   imports: [CommonModule, MaterialModule],
   exports: [LoadSpinnerComponent],
