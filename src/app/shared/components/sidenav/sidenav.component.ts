39e983630dffc478c5b6af125f38d0ebdd0ea9d1
steelvelveteen <joeyvico@gmail.com>
2022-10-04 14:46:46 +0200

    Base sidenav bar

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss'],
 })
-export class SidenavComponent {}
+export class SidenavComponent {
+  username = 'Joey Vico';
+  time = new Observable<string>((observer: Observer<string>) => {
+    setInterval(() => observer.next(new Date().toString()), 1000);
+  });
+}
