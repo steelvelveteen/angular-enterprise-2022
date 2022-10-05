35eaf11f56eaee2b70508c677aa10a1469eca4a6
steelvelveteen <joeyvico@gmail.com>
2022-10-05 20:37:31 +0200

    the body reacts to sidenav toggle

--- src/app/feature/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/feature/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { SidenavService } from 'src/app/shared/services/sidenav.service';
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
   styleUrls: ['./admin-dashboard.component.scss'],
 })
-export class AdminDashboardComponent {}
+export class AdminDashboardComponent {
+  trimBody!: boolean;
+  constructor(private sidenavService: SidenavService) {
+    this.sidenavService.toggleSidenav$.subscribe(() => {
+      this.toggleBodyWidth();
+    });
+  }
+
+  toggleBodyWidth = () => {
+    this.trimBody = !this.trimBody;
+  };
+}
