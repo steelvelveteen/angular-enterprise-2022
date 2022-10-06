4adb5ae0bedbf9c8a0d11c04d3c77efc5743c74a
steelvelveteen <joeyvico@gmail.com>
2022-10-06 19:54:50 +0200

    Cleaning all up

--- src/app/shared/components/ui/sample-ui/sample-ui.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-sample-ui',
-  templateUrl: './sample-ui.component.html',
-  styleUrls: ['./sample-ui.component.scss'],
-})
-export class SampleUiComponent {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,21 +5,15 @@ import { HeaderComponent } from './components/header/header.component';
 import { SampleComponent } from './components/sample/sample.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { BtnNavLinkComponent } from './components/ui/btn-nav-link/btn-nav-link.component';
-import { ButtonComponent } from './components/ui/button/button.component';
-import { SampleUiComponent } from './components/ui/sample-ui/sample-ui.component';
+import { HeaderSystemMenuComponent } from './components/ui/header-system-menu/header-system-menu.component';
 import { SampleDirectiveDirective } from './directives/sample-directive.directive';
 import { MaterialModule } from './material.module';
-import { SamplePipePipe } from './pipes/sample-pipe.pipe';
-import { HeaderSystemMenuComponent } from './components/ui/header-system-menu/header-system-menu.component';
 
 const routes: Routes = [];
 @NgModule({
   declarations: [
     SampleComponent,
-    SampleUiComponent,
-    ButtonComponent,
     SampleDirectiveDirective,
-    SamplePipePipe,
     SidenavComponent,
     HeaderComponent,
     BtnNavLinkComponent,
