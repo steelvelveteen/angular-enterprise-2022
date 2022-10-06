bac20d958d3040639b7796477bbeab7cdd044041
steelvelveteen <joeyvico@gmail.com>
2022-10-06 19:35:43 +0200

    getTitle, althogh a terrible idea

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,25 +1,36 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { UiService } from 'src/app/core/services/ui/ui.service';
+import { NavBtnLink, NavData } from '../sidenav/nav-data';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnDestroy {
+export class HeaderComponent implements OnInit, OnDestroy {
   toggleSidenavSubscription: Subscription = new Subscription();
   headerTitleSubscription: Subscription = new Subscription();
   trimHeader!: boolean;
   headerTitle: string = 'Dashboard';
+  location!: Location;
+  routes: NavBtnLink[] = [];
+  title: string = '';
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, location: Location) {
     this.toggleSidenavSubscription = this.uiService.toggleSidenav$.subscribe(() => {
       this.toggleHeader();
     });
     this.headerTitleSubscription = this.uiService.changeHeaderTitle$.subscribe((title: string) => {
       this.headerTitle = title;
     });
+
+    this.location = location;
+  }
+
+  ngOnInit() {
+    this.routes = NavData;
   }
 
   toggleSidenav = () => {
@@ -29,6 +40,18 @@ export class HeaderComponent implements OnDestroy {
   toggleHeader = () => {
     this.trimHeader = !this.trimHeader;
   };
+
+  getTitle(): string {
+    const titlee = this.location.prepareExternalUrl(this.location.path());
+
+    // eslint-disable-next-line no-plusplus
+    for (let item = 0; item < this.routes.length; item++) {
+      if (this.routes[item].routerLink === titlee) {
+        return this.routes[item].title;
+      }
+    }
+    return 'Dashboard';
+  }
 
   ngOnDestroy(): void {
     this.toggleSidenavSubscription.unsubscribe();
