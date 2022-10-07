df2392bbdc06e443639a278c51da506bf298b6f0
steelvelveteen <joeyvico@gmail.com>
2022-10-07 14:52:53 +0200

    Removed sample component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,16 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { SampleComponent } from './components/sample/sample.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { HeaderSystemMenuComponent } from './components/ui/header-system-menu/header-system-menu.component';
-import { SampleDirectiveDirective } from './directives/sample-directive.directive';
-import { MaterialModule } from './material.module';
 import { MenuBtnLinkComponent } from './components/ui/menu-btn-link/menu-btn-link.component';
 import { SublevelBtnLinkComponent } from './components/ui/sublevel-btn-link/sublevel-btn-link.component';
+import { SampleDirectiveDirective } from './directives/sample-directive.directive';
+import { MaterialModule } from './material.module';
 
 const routes: Routes = [];
 @NgModule({
   declarations: [
-    SampleComponent,
     SampleDirectiveDirective,
     SidenavComponent,
     HeaderComponent,
