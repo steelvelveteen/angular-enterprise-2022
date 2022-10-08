565bc1bdc76a8071f7c65a663fb4f8a1113f41f9
joeyOnMac <joeyvico@gmail.com>
2022-10-08 10:08:47 +0200

    Refactored animation to its own file

--- src/app/shared/components/ui/menu-btn-link/menu-btn-link.component.ts
+++ src/app/shared/components/ui/menu-btn-link/menu-btn-link.component.ts
@@ -1,23 +1,14 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { UiService } from 'src/app/core/services/ui/ui.service';
 import { RouteInfo } from '../../sidenav/nav-data';
+import { slideInOut } from '../animations';
 
 @Component({
   selector: 'app-menu-btn-link',
   templateUrl: './menu-btn-link.component.html',
   styleUrls: ['./menu-btn-link.component.scss'],
-  animations: [
-    trigger('enterAnimation', [
-      state(
-        'initial',
-        style({ height: '0', opacity: '0', overflow: 'hidden', visibility: 'hidden' })
-      ),
-      state('final', style({ overflow: 'hidden' })),
-      transition('initial <=> final', animate('200ms')),
-    ]),
-  ],
+  animations: [slideInOut],
 })
 export class MenuBtnLinkComponent {
   @Input() collapsed!: boolean;
