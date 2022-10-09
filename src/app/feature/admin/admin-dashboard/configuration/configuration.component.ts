8e933851aa02ecb47c9937cfc8e8da567bc1d075
steelvelveteen <joeyvico@gmail.com>
2022-10-09 19:11:16 +0200

    Linting

--- src/app/feature/admin/admin-dashboard/configuration/configuration.component.ts
+++ src/app/feature/admin/admin-dashboard/configuration/configuration.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-configuration',
   templateUrl: './configuration.component.html',
-  styleUrls: ['./configuration.component.scss']
+  styleUrls: ['./configuration.component.scss'],
 })
-export class ConfigurationComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class ConfigurationComponent {}
--- src/app/shared/components/ui/header-system-menu/header-system-menu.component.ts
+++ src/app/shared/components/ui/header-system-menu/header-system-menu.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header-system-menu',
   templateUrl: './header-system-menu.component.html',
-  styleUrls: ['./header-system-menu.component.scss']
+  styleUrls: ['./header-system-menu.component.scss'],
 })
-export class HeaderSystemMenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class HeaderSystemMenuComponent {}
