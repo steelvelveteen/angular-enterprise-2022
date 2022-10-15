20b5848af453945aa6aae70ff5d0c06f7f269d68
joeyOnMac <joeyvico@gmail.com>
2022-10-15 09:56:55 +0200

    commented out routes in ap routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,36 +2,36 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./feature/home/home.module').then(m => m.HomeModule),
-  },
-  {
-    path: 'auth',
-    loadChildren: () => import('./core/auth/auth.module').then(m => m.AuthModule),
-  },
-  {
-    path: 'admin',
-    loadChildren: () => import('./feature/admin/admin.module').then(m => m.AdminModule),
-  },
-  {
-    path: 'user',
-    loadChildren: () => import('./feature/user/user.module').then(m => m.UserModule),
-  },
-  {
-    path: '404',
-    loadChildren: () =>
-      import('./shared/components/error404/error404.module').then(m => m.Error404Module),
-  },
-  {
-    path: '500',
-    loadChildren: () =>
-      import('./shared/components/error500/error500.module').then(m => m.Error500Module),
-  },
-  {
-    path: '**',
-    redirectTo: '404',
-  },
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./feature/home/home.module').then(m => m.HomeModule),
+  // },
+  // {
+  //   path: 'auth',
+  //   loadChildren: () => import('./core/auth/auth.module').then(m => m.AuthModule),
+  // },
+  // {
+  //   path: 'admin',
+  //   loadChildren: () => import('./feature/admin/admin.module').then(m => m.AdminModule),
+  // },
+  // {
+  //   path: 'user',
+  //   loadChildren: () => import('./feature/user/user.module').then(m => m.UserModule),
+  // },
+  // {
+  //   path: '404',
+  //   loadChildren: () =>
+  //     import('./shared/components/error404/error404.module').then(m => m.Error404Module),
+  // },
+  // {
+  //   path: '500',
+  //   loadChildren: () =>
+  //     import('./shared/components/error500/error500.module').then(m => m.Error500Module),
+  // },
+  // {
+  //   path: '**',
+  //   redirectTo: '404',
+  // },
 ];
 
 @NgModule({
