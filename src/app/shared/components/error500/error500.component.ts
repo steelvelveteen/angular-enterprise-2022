7e7b8a55e0b355d5a0b1b9682ef72cf4198d3ccd
joeyOnMac <joeyvico@gmail.com>
2022-10-15 09:55:31 +0200

    Removed all modules and components

--- src/app/shared/components/error500/error500.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-error500',
-  templateUrl: './error500.component.html',
-  styleUrls: ['./error500.component.scss'],
-})
-export class Error500Component {}
--- src/app/shared/components/ui/animations.ts
+++
@@ -1,7 +0,0 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
-
-export const slideInOut = trigger('slideInOut', [
-  state('initial', style({ height: '0', opacity: '0', overflow: 'hidden', visibility: 'hidden' })),
-  state('final', style({ overflow: 'hidden' })),
-  transition('initial <=> final', animate('200ms')),
-]);
