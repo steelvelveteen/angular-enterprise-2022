962c6edc6d622fd8dc82fc3d9b5bf4f8cc8b391c
steelvelveteen <joeyvico@gmail.com>
2022-10-19 09:57:12 +0200

    Added a role guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
+import { RoleGuard } from './core/guards/role.guard';
 
 const routes: Routes = [
   {
@@ -13,7 +14,7 @@ const routes: Routes = [
   },
   {
     path: 'admin',
-    canActivate: [AuthGuard],
+    canActivate: [AuthGuard, RoleGuard],
     loadChildren: () => import('./feature/admin/admin.module').then(m => m.AdminModule),
   },
   {
--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 
+export enum Roles {
+  DEFAULT = 0,
+  ADMIN = 1,
+  USER = 2,
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   isLoggedIn = true;
+  role = Roles.ADMIN;
+
   isAuthenticated = (): boolean => this.isLoggedIn;
+  isLoggedInAsAdmin = (): boolean => this.role === Roles.ADMIN;
 }
