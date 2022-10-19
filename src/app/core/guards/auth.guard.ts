1620c5a701c76ef56db28b9f32856a6c2c62a1ce
steelvelveteen <joeyvico@gmail.com>
2022-10-19 09:42:33 +0200

    Added auth guard for admin dadshboard to reroute to login if false

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    const isLoggedIn = this.authService.isAuthenticated();
+
+    if (isLoggedIn) return true;
+
+    this.router.navigate(['auth/login']);
+    return false;
   }
 }
