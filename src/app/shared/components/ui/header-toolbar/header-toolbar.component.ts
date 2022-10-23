7086b70fe0feb63b7b7dbd604278d5faed572599
steelvelveteen <joeyvico@gmail.com>
2022-10-23 10:37:29 +0200

    Adding search advanced filtering with observables

--- src/app/shared/components/ui/header-toolbar/header-toolbar.component.ts
+++ src/app/shared/components/ui/header-toolbar/header-toolbar.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  fromEvent,
+  map,
+  Subscription,
+  tap,
+} from 'rxjs';
 import { slideInOut } from '../animations';
 
 @Component({
@@ -7,9 +16,31 @@ import { slideInOut } from '../animations';
   styleUrls: ['./header-toolbar.component.scss'],
   animations: [slideInOut],
 })
-export class HeaderToolbarComponent {
+export class HeaderToolbarComponent implements AfterViewInit {
+  @ViewChild('inputRef') inputRef!: ElementRef;
+  typeahead$: Subscription = new Subscription();
   isShowSystemMenu = false;
   isShowNotificationsMenu = false;
+
+  ngAfterViewInit(): void {
+    this.typeahead$ = fromEvent(this.inputRef.nativeElement, 'keyup')
+      .pipe(
+        tap(() => {
+          // console.log('keys pressed');
+        }),
+        map((event: any) => {
+          return event.target.value.trim();
+        }),
+        filter((searchTerm: string) => {
+          return searchTerm.length > 2;
+        }),
+        debounceTime(150),
+        distinctUntilChanged()
+      )
+      .subscribe(data => {
+        this.getFilteredUsers(data);
+      });
+  }
 
   toggleSystemMenu = (): void => {
     this.isShowSystemMenu = !this.isShowSystemMenu;
@@ -30,4 +61,19 @@ export class HeaderToolbarComponent {
     // eslint-disable-next-line no-console
     console.log(`Searching for ${value}`);
   };
+
+  getFilteredUsers(filterTerm: string) {
+    console.log(filterTerm);
+    // this.usersService.getCountries(filterTerm.trim()).subscribe(
+    // res => {
+    // if (res.length > 0) {
+    // this.isCountriesList = true;
+    // this.countries = res;
+    // }
+    // },
+    // () => {
+    // this.showCountriesError = true;
+    // }
+    // );
+  }
 }
