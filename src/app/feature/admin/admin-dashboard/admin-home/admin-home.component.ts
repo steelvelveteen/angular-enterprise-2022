8e83646418ae9f855080c77d3e51e9f26ff08500
steelvelveteen <joeyvico@gmail.com>
2022-10-23 16:37:01 +0200

    clean up admin home observable

--- src/app/feature/admin/admin-dashboard/admin-home/admin-home.component.ts
+++ src/app/feature/admin/admin-dashboard/admin-home/admin-home.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
-import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-admin-home',
   templateUrl: './admin-home.component.html',
   styleUrls: ['./admin-home.component.scss'],
 })
-export class AdminHomeComponent {
-  profileUserName$: Observable<string>;
-  constructor() {
-    this.profileUserName$ = of('Joey Vico');
-  }
-}
+export class AdminHomeComponent {}
