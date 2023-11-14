e6e8eb20ccfc52a44096fab24b5f1ab2df2d4a7e
joeyOnMac <joeyvico@gmail.com>
2023-11-14 12:30:56 +0100

    UPdatea angular core

--- src/app/core/guards/role.guard.ts
+++ src/app/core/guards/role.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class RoleGuard implements CanActivate {
+export class RoleGuard  {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(): boolean {
